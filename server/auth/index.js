76c9d4434b3fd9a6aadee4e07dbbea90a2391c7a
Tal Luigi <luigi.t.agr@gmail.com>
2020-02-22 16:51:23 -0500

    feat: refactored the api routes, auth routes, and user thunks to support the addition of the firstName and lastName fields in the User model

--- server/auth/index.js
+++ server/auth/index.js
@@ -13,7 +13,8 @@ router.get('/me', (req, res) => {
 
 router.post('/signup', async (req, res, next) => {
   try {
-    const user = await User.create(req.body)
+    const {firstName, lastName, email, password} = req.body
+    const user = await User.create({firstName, lastName, email, password})
     req.login(user, err => (err ? next(err) : res.json(user)))
   } catch (err) {
     if (err.name === 'SequelizeUniqueConstraintError') {
