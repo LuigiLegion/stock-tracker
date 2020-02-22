886fd612eab2806b2295fb57772ea04e893ac23e
Tal Luigi <luigi.t.agr@gmail.com>
2020-02-22 16:31:55 -0500

    feat: added firstName and lastName fields to the User model

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -6,6 +6,14 @@ const db = require('../db')
 
 // Model
 const User = db.define('user', {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
   email: {
     type: Sequelize.STRING,
     unique: true,
