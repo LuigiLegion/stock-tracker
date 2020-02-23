02be32db95d192898a76aba840da7d7bd03ca652
Tal Luigi <luigi.t.agr@gmail.com>
2020-02-23 14:35:39 -0500

    feat: mapped balance to the Portfolio component, converted it from cents to dollars, and fixed its value to two points after the decimal to show the current cash balance of a user

--- client/components/Portfolio.js
+++ client/components/Portfolio.js
@@ -1,9 +1,30 @@
 // Imports
 import React from 'react'
+import {connect} from 'react-redux'
+import PropTypes from 'prop-types'
 
 // Component
-export const Portfolio = () => {
-  return <div className="center">PORTFOLIO</div>
+const Portfolio = ({balance}) => {
+  const balanceInDollars = (balance / 100).toFixed(2)
+
+  return (
+    <div className="center">
+      <div>Portfolio (VALUE)</div>
+      <div>Cash - ${balanceInDollars}</div>
+    </div>
+  )
 }
 
-export default Portfolio
+// Container
+const mapStateToProps = state => {
+  return {
+    balance: state.user.balance
+  }
+}
+
+export default connect(mapStateToProps)(Portfolio)
+
+// Prop Types
+Portfolio.propTypes = {
+  balance: PropTypes.number
+}
