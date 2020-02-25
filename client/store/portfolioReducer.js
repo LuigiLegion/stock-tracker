b8ec255149c5bf30918d0cf36658ba51ec50b989
Tal Luigi <luigi.t.agr@gmail.com>
2020-02-25 15:04:44 -0500

    feat: created portfolio analysis logic in the getPortfolio thunk creator and tweaked initialState and reducer accordingly

--- client/store/portfolioReducer.js
+++ client/store/portfolioReducer.js
@@ -8,7 +8,8 @@ const REMOVED_PORTFOLIO = 'REMOVED_PORTFOLIO'
 // Initial State
 const initialState = {
   balance: 0,
-  value: 0
+  value: 0,
+  stocks: []
 }
 
 // Action Creators
@@ -25,7 +26,44 @@ export const getPortfolioThunkCreator = () => async (dispatch, getState) => {
   try {
     const {data} = await axios.get(`/api/users/${id}`)
 
-    dispatch(gotPortfolioActionCreator(data.portfolio || initialState))
+    const stocksQuantity = data.transactions.reduce((acc, curTransaction) => {
+      if (acc[curTransaction.ticker]) {
+        acc[curTransaction.ticker] =
+          acc[curTransaction.ticker] + curTransaction.quantity
+      } else {
+        acc[curTransaction.ticker] = curTransaction.quantity
+      }
+
+      return acc
+    }, {})
+
+    const stocks = []
+    for (let key in stocksQuantity) {
+      if (stocksQuantity.hasOwnProperty(key)) {
+        const price = 100000
+        const stock = {
+          ticker: key,
+          quantity: stocksQuantity[key],
+          price,
+          value: stocksQuantity[key] * price
+        }
+
+        stocks.push(stock)
+      }
+    }
+
+    const stocksTotalValue = stocks.reduce(
+      (acc, curStock) => acc + curStock.value,
+      0
+    )
+
+    const portfolio = {
+      balance: data.portfolio.balance,
+      value: stocksTotalValue,
+      stocks
+    }
+
+    dispatch(gotPortfolioActionCreator(portfolio || initialState))
   } catch (error) {
     console.error(error)
   }
@@ -38,7 +76,8 @@ export const portfolioReducer = (state = initialState, action) => {
       return {
         ...state,
         balance: action.portfolio.balance,
-        value: action.portfolio.value
+        value: action.portfolio.value,
+        stocks: action.portfolio.stocks
       }
 
     case REMOVED_PORTFOLIO:
