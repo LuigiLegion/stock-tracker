5749b959e02988e9225e29605979cdfb3119ccf7
Tal Luigi <luigi.t.agr@gmail.com>
2020-02-26 15:08:57 -0500

    feat: created the makeTransaction thunk creator in transactionReducer

--- client/store/transactionsReducer.js
+++ client/store/transactionsReducer.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 
 // Action Types
 const GOT_TRANSACTIONS = 'GOT_TRANSACTIONS'
+const MADE_TRANSACTION = 'MADE_TRANSACTION'
 const REMOVED_TRANSACTIONS = 'REMOVED_TRANSACTIONS'
 
 // Initial State
@@ -14,6 +15,11 @@ export const gotTransactionsActionCreator = transactions => ({
   transactions
 })
 
+export const madeTransactionActionCreator = transaction => ({
+  type: MADE_TRANSACTION,
+  transaction
+})
+
 export const removedPortfolioActionCreator = () => ({
   type: REMOVED_TRANSACTIONS
 })
@@ -21,10 +27,35 @@ export const removedPortfolioActionCreator = () => ({
 // Thunk Creators
 export const getTransactionsThunkCreator = () => async (dispatch, getState) => {
   const {id} = getState().user
+
   try {
     const {data} = await axios.get(`/api/users/${id}`)
 
     dispatch(gotTransactionsActionCreator(data.transactions || initialState))
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export const makeTransactionThunkCreator = (ticker, quantity) => async (
+  dispatch,
+  getState
+) => {
+  try {
+    const userId = getState().user.id
+    const {id, balance} = getState().portfolio.id
+
+    const {data} = await axios.post('/api/transactions', {
+      userId,
+      portfolioId: id,
+      balance,
+      ticker,
+      quantity
+    })
+
+    console.log({data})
+
+    dispatch(madeTransactionActionCreator(data))
   } catch (error) {
     console.error(error)
   }
