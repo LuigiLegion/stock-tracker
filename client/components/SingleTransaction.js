919d193dcdc857abd2305dba4dfbcddb547cfa37
Tal Luigi <luigi.t.agr@gmail.com>
2020-02-26 23:42:11 -0500

    feat: refactored the SingleTransaction component to bring createdAt in as a prop and parse it to show the date for each transaction

--- client/components/SingleTransaction.js
+++ client/components/SingleTransaction.js
@@ -2,12 +2,17 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const SingleTransaction = ({transaction}) => {
-  const {ticker, quantity, price} = transaction
+  const {ticker, quantity, price, createdAt} = transaction
   const priceInDollars = (price / 100).toFixed(2)
+  const transactionDate = new Date(Date.parse(createdAt))
 
   return (
     <div className="transactions-column-containee">
-      {`BUY (${ticker}) - ${quantity} Shares @ $ ${priceInDollars}/Share`}
+      <div>
+        {`BUY (${ticker}) - ${quantity} Shares @ $ ${priceInDollars}/Share`}
+      </div>
+
+      <div>{`Date of purchase - (${transactionDate})`}</div>
     </div>
   )
 }
