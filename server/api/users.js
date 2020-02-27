0fc2b605ad6e9a9177546d10ef4bb2af14f5edbb
Tal Luigi <luigi.t.agr@gmail.com>
2020-02-27 02:16:07 -0500

    chore: removed unnecessary eager loading post dedicated portfolios and transactions routers creation and unused GET / route in the users router

--- server/api/users.js
+++ server/api/users.js
@@ -2,35 +2,9 @@
 const router = require('express').Router()
 
 // Models
-const {User, Portfolio, Transaction} = require('../db/models')
+const {User} = require('../db/models')
 
 // Routes
-router.get('/', async (req, res, next) => {
-  try {
-    const users = await User.findAll({
-      // Explicitly select only the id, googleId, firstName, lastName,
-      // and email fields - even though users' passwords are encrypted,
-      // it won't help if we just send everything to anyone who asks!
-      attributes: ['id', 'googleId', 'firstName', 'lastName', 'email'],
-      include: [
-        {
-          model: Portfolio,
-          attributes: ['id', 'balance', 'value']
-        },
-        {
-          model: Transaction,
-          separate: true,
-          order: [['createdAt', 'DESC']],
-          attributes: ['id', 'ticker', 'quantity', 'price', 'createdAt']
-        }
-      ]
-    })
-    res.json(users)
-  } catch (error) {
-    next(error)
-  }
-})
-
 router.get('/:userId', async (req, res, next) => {
   const {userId} = req.params
 
@@ -39,19 +13,7 @@ router.get('/:userId', async (req, res, next) => {
       // Explicitly select only the id, googleId, firstName, lastName,
       // and email fields - even though users' passwords are encrypted,
       // it won't help if we just send everything to anyone who asks!
-      attributes: ['id', 'googleId', 'firstName', 'lastName', 'email'],
-      include: [
-        {
-          model: Portfolio,
-          attributes: ['id', 'balance', 'value']
-        },
-        {
-          model: Transaction,
-          separate: true,
-          order: [['createdAt', 'DESC']],
-          attributes: ['id', 'ticker', 'quantity', 'price', 'createdAt']
-        }
-      ]
+      attributes: ['id', 'googleId', 'firstName', 'lastName', 'email']
     })
     res.json(user)
   } catch (error) {
