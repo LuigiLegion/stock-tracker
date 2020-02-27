3fce42319a164b50d31d7d8e1c89dce97cb74ee0
Tal Luigi <luigi.t.agr@gmail.com>
2020-02-27 12:24:04 -0500

    feat: created logic based on current price and opening price to determine a stocks color and plugged said color as a class on the container div in the SingleStock component

--- client/components/SingleStock.js
+++ client/components/SingleStock.js
@@ -2,11 +2,20 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const SingleStock = ({stock}) => {
-  const {ticker, quantity, value} = stock
+  const {ticker, quantity, price, open, value} = stock
   const valueInDollars = (value / 100).toFixed(2)
 
+  let color = 'grey'
+  if (price > open) {
+    color = 'green'
+  } else if (price < open) {
+    color = 'red'
+  }
+
   return (
-    <div className="portfolio-column-containee portfolio-row-container">
+    <div
+      className={`portfolio-column-containee portfolio-row-container background-color-${color}`}
+    >
       <div className="portfolio-column-stock-containee">{`${ticker} - ${quantity} Shares`}</div>
 
       <div className="portfolio-column-price-containee">{`$ ${valueInDollars}`}</div>
