befe725c230ba1c2d3dc1a4701575e0c4dfb6c6b
Tal Luigi <luigi.t.agr@gmail.com>
2020-03-02 16:37:08 -0800

    chore: removed unnecessary code in the Portfolio component

--- client/components/Portfolio.js
+++ client/components/Portfolio.js
@@ -71,8 +71,7 @@ const mapStateToProps = state => {
   return {
     balance: state.portfolio.balance,
     value: state.portfolio.value,
-    stocks: state.portfolio.stocks,
-    stocksCount: state.portfolio.stocks.length
+    stocks: state.portfolio.stocks
   }
 }
 
