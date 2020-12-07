209baefadf8db2e00a2928ea999023f8c750c807
Tal Luigi <luigi.t.agr@gmail.com>
2020-12-07 00:16:16 -0500

    chore: updated utility function names in the utilities export file

--- client/helpers/index.js
+++ client/helpers/index.js
@@ -1,5 +1,3 @@
 // Exports
-export {default as toDollars} from './converters/toDollars'
-export {
-  default as toastNotificationGenerator
-} from './generators/toastNotificationGenerator'
+export {default as dollars} from './converters/dollars'
+export {default as toastNotification} from './generators/toastNotification'
