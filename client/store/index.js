ab0726a1a3864a895829f7609384691748b2f5ec
Tal Luigi <luigi.t.agr@gmail.com>
2020-12-17 01:17:31 -0500

    chore: replaced the combineReducers logic with the root reducer and updated export paths in the store index file

--- client/store/index.js
+++ client/store/index.js
@@ -1,29 +1,20 @@
 // Imports
-import {createStore, combineReducers, applyMiddleware} from 'redux'
+import {createStore, applyMiddleware} from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import {createLogger} from 'redux-logger'
 import {composeWithDevTools} from 'redux-devtools-extension'
 
-import userReducer from './userReducer'
-import portfolioReducer from './portfolioReducer'
-import transactionsReducer from './transactionsReducer'
-import layoutReducer from './layoutReducer'
+import rootReducer from './rootReducer'
 
 // Initializations
-const reducer = combineReducers({
-  user: userReducer,
-  portfolio: portfolioReducer,
-  transactions: transactionsReducer,
-  layout: layoutReducer
-})
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
-const store = createStore(reducer, middleware)
+const store = createStore(rootReducer, middleware)
 
 // Exports
 export default store
-export * from './userReducer'
-export * from './portfolioReducer'
-export * from './transactionsReducer'
-export * from './layoutReducer'
+export * from './reducers/userReducer'
+export * from './reducers/portfolioReducer'
+export * from './reducers/transactionsReducer'
+export * from './reducers/layoutReducer'
