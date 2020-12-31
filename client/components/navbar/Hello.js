b7e637320598ddcf4d64089c7bf55dde5c0b3a7b
Tal Luigi <luigi.t.agr@gmail.com>
2020-12-31 00:11:58 -0500

    chore: removed unnecessary export statement in the Hello component

--- client/components/navbar/Hello.js
+++ client/components/navbar/Hello.js
@@ -4,7 +4,7 @@ import {connect} from 'react-redux'
 import PropTypes from 'prop-types'
 
 // Component
-export const Hello = ({firstName, color, onClick}) => {
+const Hello = ({firstName, color, onClick}) => {
   return (
     <span className={`navbar-user-name text-color-${color}`} onClick={onClick}>
       Hello, {firstName || 'guest'}.
