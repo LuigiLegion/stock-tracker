8af4d843b6f97814a369373b4fde1d8615d19616
Tal Luigi <luigi.t.agr@gmail.com>
2021-01-01 00:42:11 -0500

    chore: added component exports and updated existing ones in the components index file

--- client/components/index.js
+++ client/components/index.js
@@ -4,11 +4,17 @@
  * This way, we can import all of our components from the same place, rather than
  * having to figure out which file they belong to!
  **/
-
-export {default as Navbar} from './navbar/Navbar'
-export {default as Hello} from './navbar/Hello'
+export {default as PageNotFound} from './404/PageNotFound'
 export {Login, Signup} from './auth/AuthForm'
 export {default as Home} from './dashboard/Home'
+export {default as Spacer} from './layout/Spacer'
+export {default as Links} from './links/Links'
+export {default as LinksBurger} from './links/LinksBurger'
+export {default as Hello} from './navbar/Hello'
+export {default as Navbar} from './navbar/Navbar'
+export {default as Preloader} from './navbar/Preloader'
+export {default as BuyForm} from './portfolio/BuyForm'
 export {default as Portfolio} from './portfolio/Portfolio'
+export {default as Stock} from './portfolio/Stock'
+export {default as Transaction} from './portfolio/Transaction'
 export {default as Transactions} from './portfolio/Transactions'
-export {default as PageNotFound} from './404/PageNotFound'
