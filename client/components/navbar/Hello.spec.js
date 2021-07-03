d2964b9f51c993f8852bc2c7d75b94a3bd6c1b5d
Tal Luigi <luigi.t.agr@gmail.com>
2021-07-03 00:25:06 -0400

    bugfix: fixed the render the first name test case in the Hello component test suite

--- client/components/navbar/Hello.spec.js
+++ client/components/navbar/Hello.spec.js
@@ -1,23 +1,26 @@
-/* global describe beforeEach it */
-
+// Imports
 import {expect} from 'chai'
-import React from 'react'
 import enzyme, {shallow} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+import React from 'react'
 
 import {Hello} from './Hello'
 
+// Initializations
 const adapter = new Adapter()
 enzyme.configure({adapter})
 
+// Tests
 describe('Hello', () => {
-  let hello
+  it('renders the first name in a span when the firstName prop is received', () => {
+    const hello = shallow(
+      <Hello
+        firstName="Cody"
+        color="white"
+        onClick={event => event.stopPropagation()}
+      />
+    )
 
-  beforeEach(() => {
-    hello = shallow(<Hello firstName="Cody" />)
-  })
-
-  it('renders the first name in a span', () => {
     expect(hello.find('span').text()).to.be.equal('Hello, Cody.')
   })
 })
